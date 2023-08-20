371567591a25022199dcae759132db9a6f889807
Sunho <sunh803@naver.com>
2023-08-21 05:01:56 +0900

    setting: XcodeProjcet생성

---
+++ JjukJjuk/JjukJjuk Watch App/JjukJjukApp.swift
@@ -0,0 +1,17 @@
+//
+//  JjukJjukApp.swift
+//  JjukJjuk Watch App
+//
+//  Created by 235 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct JjukJjuk_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JjukJjuk/JjukJjuk/JjukJjukApp.swift
@@ -0,0 +1,17 @@
+//
+//  JjukJjukApp.swift
+//  JjukJjuk
+//
+//  Created by 235 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct JjukJjukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
